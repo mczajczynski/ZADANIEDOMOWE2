36f7b2bb514ebb52a7dd11d9e79f0998ef442fb1
Maciej <m.czajczynski28@gmail.com>
2018-07-07 15:21:43 +0200

    Zadanie 1.1

---
+++ src/Krzeslo.java
@@ -0,0 +1,10 @@
+public class Krzeslo {
+
+    int nogi;
+    String kolor;
+    String ksztalt;
+
+    Krzeslo(){
+
+    }
+}
---
+++ src/Zadanie2.java
@@ -0,0 +1,19 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+
+        Krzeslo krzeslo1 = new Krzeslo();
+        Krzeslo krzeslo2 = new Krzeslo();
+
+
+        krzeslo1.kolor = "brązowy";
+        krzeslo1.ksztalt = "klasyczny";
+        krzeslo1.nogi = 4;
+
+        krzeslo2.kolor = "czerwony";
+        krzeslo2.ksztalt = "awangardowy";
+        krzeslo2.nogi = 2;
+
+        System.out.println("Krzesło 1 " + "Kolor: " + krzeslo1.kolor + " Kształt: " + krzeslo1.ksztalt + " Ilość nog: " + krzeslo1.nogi);
+        System.out.println("Krzesło 2 " + "Kolor: " + krzeslo2.kolor + " Kształt: " + krzeslo2.ksztalt + " Ilość nog: " + krzeslo2.nogi);
+    }
+}
