139e16f8091c9f3f38bcc38da9dbd87e720574ec
Maciej <m.czajczynski28@gmail.com>
2018-07-08 20:48:28 +0200

    Zadanie 2.2 FINAL version

---
+++ src/zadanie3.java
@@ -0,0 +1,16 @@
+public class zadanie3 {
+
+
+    public static void main(String[] args) {
+        System.out.println(
+                "1. Klasa to szablonem pozwalający definiować własne, bardziej złożone typy danych i pozwalaja zdefiniowac cechy oraz funkconalnsoci.\n" +
+                "2. Obiekty to  konkretny obiekt klasy i jest tworzyony podobnie do zwykłych zmiennych –poprzez deklarację i inicjalizację. Klasy traktujemy jako nowe, bardziej złożone typy danych. Inicjalizacja polega na utworzeniu obiektu przez wywołanie operatora new.\n" +
+                "3. Zmienna to funkcja ktora pozwala nadać wartości nazwę a następnie się do niej po tej nazwie odwoływać.\n" +
+                "4. Kazda zmienna musi miec okreslony typ, np int albo String.\n" +
+                "5. W Klasie mogą się zajmować pola, metody i obiekty\n" +
+                "6. Konstruktory domyślne sa wywolywane przez operator new i sluza do tworzenia nowych obiektow\n" +
+                "7. Jesli w klasie zdefiniowano chociaz jeden własny kontruktor to kompilator nie wygeneruje dla nas konstruktora domyslengo.\n" +
+                "8. car1 I car2 to dwie referencje wskazujące na ten sam model auta\n");
+
+    }
+}
